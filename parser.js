b7425e3641f9ad5d6b45499d967bee01f9f01b25
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-04-16 22:55:51 -0700

    Parse ISO 8601 timestamps

--- parser.js
+++ parser.js
@@ -1,7 +1,32 @@
 export function parse(icsText)
 {
     const icsLines = icsText.split("\n");
-     
+    
+       
+}
+
+function parse8601withTime(timestamp)
+{
+    // ymd
+    const year = parseInt(timestamp.substring(0, 4));
+    const month = parseInt(timestamp.substring(4, 6)) - 1; // for some weird ass reason the Date() constructor interprets 0 to be January
+    const day = parseInt(timestamp.substring(6, 8));
+
+    // time
+    const hour = parseInt(timestamp.substring(9, 11)); // 24hr format
+    const minute = parseInt(timestamp.substring(11, 13));
+    const second = parseInt(timestamp.substring(13, 15));
+
+    return new Date(year, month, day, hour, minute, second, 0);
+}
+
+function parse8601withoutTime(timestamp)
+{
+    const year = parseInt(timestamp.substring(0, 4));
+    const month = parseInt(timestamp.substring(4, 6)) - 1; // for some weird ass reason the Date() constructor interprets 0 to be January
+    const day = parseInt(timestamp.substring(6, 8));
+
+    return new Date(year, month, day, 0, 0, 0, 0);
 }
 
 function getClassName(summaryLine) {
