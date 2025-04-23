eed15b4b4db6e5723944b698e01c4d462cb41503
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-04-23 13:37:16 -0700

    use a stored schedule so i dont get rate limiteed lol

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 import { setCookie, deleteCookie, getSchedule } from "./requester.js";
+import { testSchedule } from "./schedule.js";
 const scheduleTable = document.getElementById("scheduletable");
-const schedule = await getSchedule();
+// const schedule = await getSchedule();
+
+const schedule = testSchedule();
+
 console.log(schedule)
 
 for(let scheduledItem of schedule)
 {
-    console.log("test")
     let row = document.createElement("div");
     row.classList.add("flexTr");
 
     let time = makeFlexTd("timeColumn");
     let block = makeFlexTd("blockColumn");
     let classActivity = makeFlexTd("classColumn");
+
+    time.innerHTML = scheduledItem
 
     row.appendChild(time);
     row.appendChild(block);
